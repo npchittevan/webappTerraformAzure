a14509a6304b158064af9aa790cfe23320fa64db
npchittevan <nilchittevan@gmail.com>
2025-12-11 12:35:16 +0530

    added output "virtual_network_id"

--- modules/vnet/output.tf
+++ modules/vnet/output.tf
@@ -6,3 +6,7 @@ output "virtual_network_interface_ids" {
   description = "IDs of the virtual network interfaces"
   value       = azurerm_network_interface.network_interface[*].id
 }
+output "virtual_network_id" {
+  description = "ID of the virtual network"
+  value       = azurerm_virtual_network.appvnet.id
+}
